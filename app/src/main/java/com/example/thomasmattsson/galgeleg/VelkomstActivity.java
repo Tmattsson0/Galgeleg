9230be4759144716bfd2cfb3791279259f8cf89e
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-10-08 14:21:22 +0200

    First commit

---
+++ app/src/main/java/com/example/thomasmattsson/galgeleg/VelkomstActivity.java
@@ -0,0 +1,13 @@
+package com.example.thomasmattsson.galgeleg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VelkomstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
