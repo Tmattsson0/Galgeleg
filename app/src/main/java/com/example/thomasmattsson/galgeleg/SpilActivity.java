e54b2a7c5ad006d5f9ee163710b0867b80e7764f
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-10-08 15:21:40 +0200

    Added second activity and app title.

---
+++ app/src/main/java/com/example/thomasmattsson/galgeleg/SpilActivity.java
@@ -0,0 +1,13 @@
+package com.example.thomasmattsson.galgeleg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spil);
+    }
+}
--- app/src/main/java/com/example/thomasmattsson/galgeleg/VelkomstActivity.java
+++ app/src/main/java/com/example/thomasmattsson/galgeleg/VelkomstActivity.java
@@ -1,14 +1,34 @@
 package com.example.thomasmattsson.galgeleg;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 //Hej
-public class VelkomstActivity extends AppCompatActivity {
+public class VelkomstActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_velkomst);
+
+        //FindView
+        button1 = (Button) findViewById(R.id.button);
+
+        //OnClick
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == button1) {
+            Intent i = new Intent(this, SpilActivity.class);
+            startActivity(i);
+        }
     }
 }
