16d0e6f71c4d85ec890039f2c6292dab55c6244f
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-11-07 11:33:46 +0100

    Added a simple list

--- app/src/main/java/com/example/thomasmattsson/galgeleg/HighscoreActivity.java
+++ app/src/main/java/com/example/thomasmattsson/galgeleg/HighscoreActivity.java
@@ -2,14 +2,33 @@ package com.example.thomasmattsson.galgeleg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class HighscoreActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
 
-        //https://willowtreeapps.com/ideas/android-fundamentals-working-with-the-recyclerview-adapter-and-viewholder-pattern/
+        SimpleAdapter adapter = new SimpleAdapter(generateSimpleList());
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.simple_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<SimpleViewModel> generateSimpleList() {
+        List<SimpleViewModel> simpleViewModelList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            simpleViewModelList.add(new SimpleViewModel(String.format(Locale.US, "This is item %d", i)));
+        }
+
+        return simpleViewModelList;
     }
 }
---
+++ app/src/main/java/com/example/thomasmattsson/galgeleg/SimpleViewModel.java
@@ -0,0 +1,21 @@
+package com.example.thomasmattsson.galgeleg;
+
+import android.support.annotation.NonNull;
+
+public class SimpleViewModel {
+    private String simpleText;
+
+    public SimpleViewModel(@NonNull final String simpleText) {
+        setSimpleText(simpleText);
+    }
+
+    @NonNull
+    public String getSimpleText() {
+        return simpleText;
+    }
+
+    public void setSimpleText(@NonNull final String simpleText) {
+        this.simpleText = simpleText;
+    }
+
+}
