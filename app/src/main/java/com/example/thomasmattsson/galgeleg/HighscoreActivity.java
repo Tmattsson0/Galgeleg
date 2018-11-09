6f47e2f735b9204b87cfd1cbf7ab376db1bae9c1
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-11-09 09:11:35 +0100

    Highscore list now is i shared preferences
    You can also add your own name and score from the wonActicity.

--- app/src/main/java/com/example/thomasmattsson/galgeleg/HighscoreActivity.java
+++ app/src/main/java/com/example/thomasmattsson/galgeleg/HighscoreActivity.java
@@ -1,10 +1,15 @@
 package com.example.thomasmattsson.galgeleg;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -20,21 +25,7 @@ import static java.util.Comparator.comparing;
 
 public class HighscoreActivity extends AppCompatActivity {
 
-    ArrayList<Player> playerList = new ArrayList<>();
-
-    Player player1 = new Player("Thomas", 50000);
-    Player player2 = new Player("Mikkel", 14124);
-    Player player3 = new Player("Lars", 25415);
-    Player player4 = new Player("Fie", 15352);
-    Player player5 = new Player("Søren", 78634);
-    Player player6 = new Player("Julie", 42542);
-    Player player7 = new Player("Bergitte", 78374);
-    Player player8 = new Player("Theodore", 78375);
-    Player player9 = new Player("Alex", 45348);
-    Player player10 = new Player("Petra", 12348);
-    Player player11 = new Player("Børge", 74374);
-    Player player12 = new Player("Kasper", 47527);
-
+    ArrayList<Player> playerList;
     RecyclerView mRecyclerView;
 
 
@@ -42,6 +33,9 @@ public class HighscoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
+
+        playerList = loadData();
+        Collections.sort(playerList, comparing(Player::getScore).reversed());
 
         mRecyclerView = (RecyclerView) findViewById(R.id.simple_recyclerview);
         RecyclerView.LayoutManager mLayoutManager;
@@ -52,23 +46,21 @@ public class HighscoreActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
-        //Clear list
-        playerList.clear();
-
-        playerList.add(player1);
-        playerList.add(player2);
-        playerList.add(player3);
-        playerList.add(player4);
-        playerList.add(player5);
-        playerList.add(player6);
-        playerList.add(player7);
-        playerList.add(player8);
-        playerList.add(player9);
-        playerList.add(player10);
-        playerList.add(player11);
-        playerList.add(player12);
-        Collections.sort(playerList, comparing(Player::getScore).reversed());
 
 
+
+    }
+
+    private ArrayList<Player> loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("score_list", null);
+        Type type = new TypeToken<ArrayList<Player>>() {}.getType();
+        playerList = gson.fromJson(json, type);
+
+        if (playerList == null){
+            playerList = new ArrayList<>();
+        }
+        return playerList;
     }
 }
