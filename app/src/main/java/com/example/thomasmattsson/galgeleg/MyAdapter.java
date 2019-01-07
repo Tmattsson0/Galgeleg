d987566bce73ed2fa65dac44efc7ff043875ab0c
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2019-01-07 20:26:47 +0100

    Rename class

--- app/src/main/java/com/example/thomasmattsson/galgeleg/MyAdapter.java
+++ app/src/main/java/com/example/thomasmattsson/galgeleg/MyAdapter.java
@@ -22,7 +22,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @NonNull
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_simple_itemview, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.highscore_itemview, parent, false);
         return new ViewHolder(view);
     }
 
